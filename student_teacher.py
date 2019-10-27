89b68835245451c3bd634c908a1253d23fd46894
qghvnxm <56299354+qghvnxm@users.noreply.github.com>
2019-10-27 16:32:07 +0800

    类和collection模块 老师学生 成绩

--- student_teacher.py
+++ student_teacher.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+import sys
+from collections import Counter
 class Person(object):
     """
-    ????????? Person ??
+    返回具有给定名称的 Person 对象
     """
 
     def __init__(self, name):
@@ -10,44 +11,71 @@ class Person(object):
 
     def get_details(self):
         """
-        ??????????
+        返回包含人名的字符串
         """
         return self.name
-
+    def get_grade():
+        return 0
 
 class Student(Person):
     """
-    ?? Student ????? name, branch, year 3 ???
+    返回 Student 对象，采用 name, branch, year 3 个参数
     """
 
-    def __init__(self, name, branch, year):
+    def __init__(self, name, branch, year,grade):
         Person.__init__(self, name)
         self.branch = branch
         self.year = year
-
+        self.grade = grade
     def get_details(self):
         """
-        ??????????????
+        返回包含学生具体信息的字符串
         """
         return "{} studies {} and is in {} year.".format(self.name, self.branch, self.year)
-
+    def get_grade(self):
+        common=Counter(self.grade).most_common(4)
+        n1=n2=0
+        for item in common:
+            if item[0]!='D':
+                n1+=item[1]
+            else:
+                n2+=item[1]
+        print ("Pass: {}, Fail: {}".format(n1,n2))
 
 class Teacher(Person):
     """
-    ?? Teacher ??????????????
+    返回 Teacher 对象，采用字符串列表作为参数
     """
-    def __init__(self, name, papers):
+    def __init__(self, name, papers,grade):
         Person.__init__(self, name)
         self.papers = papers
-
+        self.grade = grade
     def get_details(self):
         return "{} teaches {}".format(self.name, ','.join(self.papers))
-
+    def get_grade(self):
+        common=Counter(self.grade).most_common(4)
+        n1=n2=n3=n4=0
+        for item in common:
+            if item[0]=='A':
+                n1+=item[1]
+            elif item[0]=='B':
+                n2+=item[1]
+            elif item[0]=='C':
+                n3+=item[1]
+            else:
+                n4+=item[1]
+        print('A: {}, B: {}, C: {}, D: {}'.format(n1,n2,n3,n4))
+#the Second way use join
+#s=[]
+#for i,j in common
+#	s.append("{}: {}".format(i,j))
+#print(', '.join(s))
 
 person1 = Person('Sachin')
-student1 = Student('Kushal', 'CSE', 2005)
-teacher1 = Teacher('Prashad', ['C', 'C++'])
+if sys.argv[1] == "student":
+    student1 = Student('Kushal', 'CSE', 2005, sys.argv[2])
+    student1.get_grade()
+else:
+    teacher1 = Teacher('Prashad', ['C', 'C++'], sys.argv[2])
+    teacher1.get_grade()
 
-print(person1.get_details())
-print(student1.get_details())
-print(teacher1.get_details())
